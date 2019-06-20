a2c138feea5cbdf922a4e13c2074ce75d0235607
Garry Zhang <garry_zzz@yahoo.com>
2019-06-21 09:03:37 +1000

    week2 exe2,3

--- week2/exercise3.py
+++ week2/exercise3.py
@@ -7,8 +7,14 @@ def is_odd(a_number):
 
     Look into modulo division using the '%' operator as one way of doing this.
     """
-    return None
-
+    answer = None
+    if a_number % 2 == 0: 
+        # it's even 
+        answer = False
+    else:
+        # it's odd
+        return True
+    
 
 def fix_it(moves=True, should_move=True):
     """Decide what to do.
@@ -25,7 +31,16 @@ def fix_it(moves=True, should_move=True):
     Most people write this function with 4 return statements. 
     As an extra challenge, see if you can get that down to three.
     """
-    return None
+  
+    if moves == True and should_move == False: 
+        return "Duct Tape"
+    
+    elif moves == False and should_move == True: 
+        return "WD-40"
+
+    else: 
+        return  "No Problem"
+       
 
 
 def loops_1a():
@@ -35,7 +50,7 @@ def loops_1a():
     return a list of 10 items, each one a string with exacly one star in it.
     E.g.: ['*', '*', '*', '*', '*', '*', '*', '*', '*', '*']
     """
-    return None
+    return loops_1a - tenStar
 
 
 def loops_1c(number_of_items=5, symbol="#"):
@@ -45,7 +60,7 @@ def loops_1c(number_of_items=5, symbol="#"):
     string with exacly one symbol in it.
     E.g.: ['#', '#', '#', '#', '#']
     """
-    return None
+    return loops_1c - 5 "#"
 
 
 def loops_2():
@@ -66,7 +81,7 @@ def loops_2():
             ['*', '*', '*', '*', '*', '*', '*', '*', '*', '*'],
           ]
     """
-    return None
+    return loops_2 10 * 10 stars
 
 
 def loops_3():
@@ -90,7 +105,7 @@ def loops_3():
     TIP: notice that this needs to to return strings of numbers,
          so call str(number) to cast.
     """
-    return None
+    return loops_3 - qo 
 
 
 def loops_4():
@@ -110,7 +125,7 @@ def loops_4():
       ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
     ]
     """
-    return None
+    return loops_4 - 10
 
 
 def loops_5():
